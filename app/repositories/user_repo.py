8de44363cc63f705ff49f9b602095f4ac9ac9fc7
Mohamed Radwan <1mohammed.radwan@gmail.com>
2025-12-07 23:49:30 +0200

    fix: update user repository methods to use model_dump for data serialization

--- app/repositories/user_repo.py
+++ app/repositories/user_repo.py
@@ -12,16 +12,18 @@ class UserRepository(IRepository):
 
     def add(self, user: User) -> str:
         self.logger.info(f"adding new user: {user.email}")
-        self.collection.insert_one(user.to_dict())
+        self.collection.insert_one(user.model_dump(by_alias=True))
         return user.user_id
 
     def get_by_id(self, user_id: str) -> Optional[dict]:
         self.logger.debug(f"fetching user id: {user_id}")
-        return self.collection.find_one({"_id": user_id})
+        data = self.collection.find_one({"_id": user_id})
+        return User(**data) if data else None
 
     def get_by_email(self, email: str) -> Optional[dict]:
         self.logger.debug(f" get user by email: {email}")
-        return self.collection.find_one({"email": email})
+        data = self.collection.find_one({"email": email})
+        return User(**data) if data else None
 
     def update(self, user_id: str, data: dict):
         self.logger.info(f"updating user {user_id}")
