7c88f692e8a3c2be4d1c970d30882da78684543b
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2025-12-20 09:58:13 +0200

    feat: enhance error handling with custom error page and back navigation

--- app/utils/errors.py
+++ app/utils/errors.py
@@ -1,4 +1,5 @@
-from flask import jsonify
+from flask import jsonify, render_template
+from flask_jwt_extended import current_user
 
 from app.utils.exceptions import AppError
 
@@ -6,4 +7,12 @@ from app.utils.exceptions import AppError
 def register_error_handlers(app):
     @app.errorhandler(AppError)
     def handle_app_error(e: AppError):
-        return jsonify({"error": e.message, "details": e.payload}), e.status_code
+        return (
+            render_template(
+                "error.html",
+                error_code=e.status_code,
+                error_message=e.message,
+                current_user=current_user,
+            ),
+            e.status_code,
+        )
