c35008128ef8afe1cb0bd94cf21d0fca11b1bf96
Mohamed Radwan <1mohammed.radwan@gmail.com>
2025-12-20 20:51:50 +0200

    feat: add confirmation_requested and transaction_confirmed signals with corresponding handlers

--- app/events/observers.py
+++ app/events/observers.py
@@ -1,5 +1,10 @@
-from app.events.signals import expense_created, invite_sent
-from app.models import Expense, GroupSchema, NotificationSchema, User
+from app.events.signals import (
+    confirmation_requested,
+    expense_created,
+    invite_sent,
+    transaction_confirmed,
+)
+from app.models import Expense, GroupSchema, NotificationSchema, Transaction, User
 from app.repositories import NotificationRepository
 
 
@@ -37,6 +42,38 @@ def handle_User_invite(sender, group_schema: GroupSchema, user: User, **extra):
     notify_repo.add(notification)
 
 
+def handle_confirmation_requested(sender, transaction: Transaction, **extra):
+    notify_repo = NotificationRepository()
+
+    message = (
+        f"{transaction.payer.name} has made a transaction of {transaction.amount}EGP."
+        f"Did you receive it?"
+    )
+
+    notification = NotificationSchema(
+        user_id=transaction.receiver.user_id,
+        message=message,
+        type="confirmation",
+        payload={"transaction_id": transaction.transaction_id},
+    )
+    notify_repo.add(notification)
+
+
+def handle_transaction_confirmed(sender, transaction: Transaction, **extra):
+    notify_repo = NotificationRepository()
+
+    message = f"Your transaction of {transaction.amount}EGP to {transaction.receiver.name} has been confirmed."
+
+    notification = NotificationSchema(
+        user_id=transaction.payer.user_id,
+        message=message,
+        payload={"transaction_id": transaction.transaction_id},
+    )
+    notify_repo.add(notification)
+
+
 def enable_notifications(app):
     expense_created.connect(handle_involved_users)
     invite_sent.connect(handle_User_invite)
+    confirmation_requested.connect(handle_confirmation_requested)
+    transaction_confirmed.connect(handle_transaction_confirmed)
